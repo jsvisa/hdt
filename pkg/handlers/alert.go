a21467d8a893bf7f6421b1ce994677aac0287369
jsvisa <delweng@gmail.com>
2023-06-05 15:10:02 +0800

    handlers/alert: return 200 instead of 201
    Signed-off-by: jsvisa <delweng@gmail.com>

--- pkg/handlers/alert.go
+++ pkg/handlers/alert.go
@@ -26,13 +26,15 @@ func (h handler) AddAlert(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Info("recv alerts", "#alert", len(rpcAlert.Alerts))
-	// Append to the Books table
-	if result := h.DB.CreateInBatches(&rpcAlert.Alerts, 10); result.Error != nil {
-		log.Error("failed to save alerts into db", "err", result.Error)
+	if len(rpcAlert.Alerts) > 0 {
+		// Append to the alerts table
+		if result := h.DB.CreateInBatches(&rpcAlert.Alerts, 10); result.Error != nil {
+			log.Error("failed to save alerts into db", "err", result.Error)
+		}
 	}
 
-	// Send a 201 created response
+	// Send a 200 OK
 	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode("Created")
 }
